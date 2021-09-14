0266b05f4c89e410dfaf2aead5b1a84185ad0f51
Leekibeom <lkb951103@gmail.com>
2021-09-14 16:59:24 +0900

    Todo-localstorge-Fixed, object-text,id

--- js/todo.js
+++ js/todo.js
@@ -1,19 +1,51 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
+const TODOS_KEY = "toDo";
+let toDos = [];
+const saveToDos = localStorage.getItem(TODOS_KEY);
+
+todoForm.addEventListener("submit", handletodoSubmit);
+
+function deleteToDo(event) {
+  const delTarget = event.target.parentElement;
+  delTarget.remove();
+}
 
 function addTodo(newTodo) {
   const todoBox = document.createElement("li");
+  todoBox.id = newTodo.id;
   const todo = document.createElement("span");
-  todoList.append(todoBox);
-  todoBox.append(todo);
-  todo.innerText = newTodo;
+  todo.innerText = newTodo.text;
+  const delButton = document.createElement("button");
+  todoList.appendChild(todoBox);
+  todoBox.appendChild(todo);
+  todoBox.appendChild(delButton);
+  delButton.addEventListener("click", deleteToDo);
+  delButton.innerText = "❌";
 }
 
 function handletodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
-  addTodo(newTodo);
+  const newTodoOBJ = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoOBJ);
+  saveToDo();
+  addTodo(newTodoOBJ);
   todoInput.value = ""; //Enter입력시 input값이 비워짐
 }
-todoForm.addEventListener("submit", handletodoSubmit);
+
+function saveToDo(item) {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  //JSON.stringify 값을 String타입으로 변환
+  //JSON.parse String값을 배열로 변환
+}
+
+if (saveToDos !== null) {
+  const parseToDos = JSON.parse(saveToDos);
+  toDos = parseToDos;
+  parseToDos.forEach(addTodo);
+}
