007dfc884068a8f1f6a9e9dd07645a458a7bb28e
Leekibeom <lkb951103@gmail.com>
2021-09-18 21:31:13 +0900

    back-del

--- js/border.js
+++
@@ -1,5 +0,0 @@
-const colors = ["rgba(255,0,0,0.4)", "rgba(0,255,0,0.4)", "rgba(0,0,255,0.4)"];
-const randColor = colors[Math.floor(Math.random() * colors.length)];
-
-const loginBack = document.querySelector("#user-box");
-loginBack.style.backgroundColor = `${randColor}`;
--- js/clock.js
+++ js/clock.js
@@ -1,31 +1,26 @@
-const clock = document.querySelector("#clock");
-const yearid = document.querySelector("#year");
-const monthid = document.querySelector("#month");
-const daysid = document.querySelector("#day");
-const dailyid = document.querySelector("#daily");
+const clock = document.querySelector("#date-clock");
+const date = document.querySelector("#date");
 const dailyArr = ["일", "월", "화", "수", "목", "금", "토"];
 
 function getClock() {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
+  const data = new Date();
+  const hours = String(data.getHours()).padStart(2, "0");
   //String형으로 변경 한 시간을 받아온 후 padStart = 스트링이 2자리가 아니면 앞에 0을 추가
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = hours + ":" + minutes + ":" + seconds;
+  const minutes = String(data.getMinutes()).padStart(2, "0");
+  const seconds = String(data.getSeconds()).padStart(2, "0");
+  const daily = dailyArr[data.getDay()];
+  clock.innerText = `${hours} : ${minutes} : ${seconds} / ${daily}요일`;
 }
-
-getClock();
-setInterval(getClock, 1000); //매 ms마다 지정한 함수를 호출시킨다
 
 function getDay() {
-  const date = new Date();
-  const year = String(date.getFullYear());
-  yearid.innerText = `${year}년`;
-  const month = String(date.getMonth()).padStart(2, "0");
-  monthid.innerText = `${month}월`;
-  const days = String(date.getDay()).padStart(2, "0");
-  daysid.innerText = `${days}일`;
-  const daily = dailyArr[date.getDay()];
-  dailyid.innerText = `${daily}요일`;
+  const data = new Date();
+  const year = String(data.getFullYear());
+  const month = String(data.getMonth()).padStart(2, "0");
+  const days = String(data.getDay()).padStart(2, "0");
+  date.innerText = `${year}년 ${month}월 ${days}일`;
 }
+
 getDay();
+getClock();
+setInterval(getDay, 1100);
+setInterval(getClock, 1000); //매 ms마다 지정한 함수를 호출시킨다
